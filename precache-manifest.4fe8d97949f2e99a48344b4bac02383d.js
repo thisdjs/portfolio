7fc4924f0a3743c89a3736862c8a999e1b60fa56
David Santos <d_santos@regra.pt>
2019-05-10 11:31:41 +0100

    Updates

---
+++ precache-manifest.4fe8d97949f2e99a48344b4bac02383d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a29ffbdc2e0da5516a44",
+    "url": "/static/js/main.a29ffbdc.chunk.js"
+  },
+  {
+    "revision": "724cedb511fc533c6789",
+    "url": "/static/js/2.724cedb5.chunk.js"
+  },
+  {
+    "revision": "a29ffbdc2e0da5516a44",
+    "url": "/static/css/main.794c55db.chunk.css"
+  },
+  {
+    "revision": "3b0e6ca11e35728104f2b7afe79f95bc",
+    "url": "/index.html"
+  }
+];
