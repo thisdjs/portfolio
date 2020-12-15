19d657ceeec4dc5f2ad9ea0040472d28f03cee19
david <this.djs.19@gmail.com>
2020-12-15 19:21:28 +0000

    rebuilding - removed packages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,104 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
-import AppNavBar from "./Components/AppNavBar"
-import { Grid, Typography, IconButton } from '@material-ui/core'
-import BorderWrapper from 'react-border-wrapper'
-import { FullPage, Slide } from 'react-full-page';
-import StyledSection from './Components/StyledSection'
 
 class App extends Component {   
   render() {
 
-    navigator.geolocation.getCurrentPosition((position) => console.log(position))
-
     return (
       <div id="App" className="App">
 
-        <AppNavBar>
-
-          <FullPage>
-            <Slide>
-              <StyledSection>
-                <Grid container spacing={32} style={{margin:"0px"}}>
-                    <Grid item xs={12} sm={4}>
-                      <Grid item xs style={{textAlign:"center", /*padding:"10px"*/}}>
-                        <img className="avatar" height={"150px"} src="profilePic.jpg" alt="profile" 
-                          onClick={()=>speechSynthesis.speak(new SpeechSynthesisUtterance('I like coffe'))}>
-                        </img>
-                      </Grid>
-                      <Grid item xs style={{textAlign:"center" /*, padding:"10px"*/}}>
-                        <IconButton color="inherit"  onClick={() => window.open("https://github.com/thisdjs")}>
-                          <span 
-                            className="fab fa-github" 
-                            style={{color:"#282c34", cursor: "pointer"}}>
-                          </span>
-                        </IconButton>
-                        <IconButton color="inherit" onClick={() => console.log("clicked")}>
-                          <span 
-                            className="fab fa-linkedin" 
-                            style={{color:"#282c34", cursor: "pointer"}}>
-                          </span>
-                        </IconButton>
-                        <IconButton color="inherit" onClick={() => console.log("clicked")}>
-                          <span 
-                            className="fab fa-skype" 
-                            style={{color:"#282c34", cursor: "pointer"}}>
-                          </span>
-                        </IconButton>
-                        <IconButton color="inherit" onClick={() => console.log("clicked")}>
-                          <span 
-                            className="fab fa-gitlab" 
-                            style={{color:"#282c34", cursor: "pointer"}}>
-                          </span>
-                        </IconButton>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={12} sm={8}>
-                      <div className="texts">
-                        <BorderWrapper borderWidth="2px" borderColour="#22C5EE" borderType="outset">
-                          <Typography variant="h5">Hello!! 👋</Typography>
-                          <hr/>
-                          <Typography variant="subtitle1"> 
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                          </Typography>
-                        </BorderWrapper>
-                      </div>
-                    </Grid>
-                </Grid> 
-              </StyledSection>
-            </Slide>
-            
-            <Slide style={{backgroundColor:"blue"}}>
-              <StyledSection>
-                <Grid container spacing={32} style={{margin:"0px"}}>
-                  <h1>tecnologies</h1>
-                  <img style={{height:"250px"}} 
-                    src="https://images.unsplash.com/photo-1556596768-6d27210a5ee2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80">
-                  </img>
-                </Grid>
-                {/* <div class="LI-profile-badge"  
-                  data-version="v1" 
-                  data-size="medium" 
-                  data-locale="en_US" 
-                  data-type="horizontal" 
-                  data-theme="dark" 
-                  data-vanity="david-j-santos">                  
-                  <a 
-                    href='https://pt.linkedin.com/in/david-j-santos?trk=profile-badge'
-                    target="_blank">
-                      David Santos
-                  </a>
-                </div> */}
-              </StyledSection>
-            </Slide>
-          </FullPage>
-
-        </AppNavBar>
+        <p>I make content for the web.</p>
+        
       </div>
     );
   }
